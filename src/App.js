2f3f912aedf6bd9bef39c1ed26090d20672bdbac
zhangyucong02 <zhangyucong02@58.com>
2021-11-03 19:37:26 +0800

    feat:初始化首页

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { GlobalStyle } from './style';
-import { renderRoutes } from 'react-router-config'; //renderRoutes 读取路由配置转化为 Route 标签
+import { renderRoutes } from 'react-router-config'; //renderRoutes 读取路由配置转化为 Route 标签【只能渲染一层路由】
 import { IconStyle } from './assets/iconfont/iconfont';
 import routes from './routes/index';
 import { HashRouter } from 'react-router-dom';
--- src/application/Home/index.js
+++ src/application/Home/index.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { renderRoutes } from 'react-router-config';
+import { Top } from './style';
 
 function Home(props) {
+    const { route }=props
     return (
-        <div>Home</div>
+        <div>
+            <Top>
+                <span className="iconfont menu">&#xe65c;</span>
+                <span className="title">WebApp</span>
+                <span className="iconfont search">&#xe62b;</span>
+            </Top>
+            { renderRoutes(route.routes) }
+        </div>
     )
 }
 
