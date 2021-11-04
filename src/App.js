46ad8c04257f614d08fdd307c8df3d70b3b242e7
zhangyucong02 <zhangyucong02@58.com>
2021-11-04 20:59:09 +0800

    fix:初始化store

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { GlobalStyle } from './style';
 import { renderRoutes } from 'react-router-config'; //renderRoutes 读取路由配置转化为 Route 标签【只能渲染一层路由】
 import { IconStyle } from './assets/iconfont/iconfont';
+import store from './store';
 import routes from './routes/index';
 import { HashRouter } from 'react-router-dom';
 
 
 function App() {
   return (
-    <HashRouter>
-      <GlobalStyle></GlobalStyle>
-      <IconStyle></IconStyle>
-      { renderRoutes (routes) }
-      {/* <i className="iconfont">&#xe62b;</i> */}
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        { renderRoutes (routes) }
+        {/* <i className="iconfont">&#xe62b;</i> */}
+      </HashRouter>
+    </Provider>
+    
   );
 }
 
