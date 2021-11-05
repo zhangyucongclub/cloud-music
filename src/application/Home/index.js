96417274cffb86efafb250113f7ebe7d6b57c728
zhangyucong02 <zhangyucong02@58.com>
2021-11-05 14:03:10 +0800

    fix:推荐列表轮播图

--- src/application/Home/index.js
+++ src/application/Home/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { renderRoutes } from 'react-router-config';
-import { Top } from './style';
+import { Top,Tab,TabItem } from './style';
+import { NavLink } from 'react-router-dom';// 利用 NavLink 组件进行路由跳转
 
 function Home(props) {
     const { route }=props
@@ -11,6 +12,11 @@ function Home(props) {
                 <span className="title">WebApp</span>
                 <span className="iconfont search">&#xe62b;</span>
             </Top>
+            <Tab>
+                <NavLink to="/recommend" activeClassName="selected"><TabItem><span > 推荐 </span></TabItem></NavLink>
+                <NavLink to="/singers" activeClassName="selected"><TabItem><span > 歌手 </span></TabItem></NavLink>
+                <NavLink to="/rank" activeClassName="selected"><TabItem><span > 排行榜 </span></TabItem></NavLink>
+            </Tab>
             { renderRoutes(route.routes) }
         </div>
     )
